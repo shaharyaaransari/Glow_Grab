44d6f05e0cb4c9693bd2dde2edbf38f50ec3ce49
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-06-15 10:28:58 +0530

    NavBar Update

---
+++ src/Components/NavBarOptions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBarOptions = () => {
+  return <div>NavBarOptions</div>;
+};
+
+export default NavBarOptions;
---
+++ src/Components/NavBarSearch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBarSearch = () => {
+  return <div>NavBarSearch</div>;
+};
+
+export default NavBarSearch;

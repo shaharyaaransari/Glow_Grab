1bfad8d7ce49ffcce8d06623e63cbeec951d87ff
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-06-15 20:12:02 +0530

    Update

--- src/Pages/ProductPage.tsx
+++ src/Pages/ProductPage.tsx
@@ -1,8 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Box, Text, Input } from "@chakra-ui/react";
+
+// Styles Components
+import * as css from "../Styles/ProductPageStyles";
+import { ProductsCont } from "../Styles/ProductPageStyles";
+import { useSelector } from "react-redux";
 
 const ProductPage = () => {
-  return <div>Product Page</div>;
+  const data = useSelector((store: any) => store.API_URL);
+  console.log(data);
+
+  return <ProductsCont>Product Page</ProductsCont>;
 };
 
 export default ProductPage;
--- src/Styles/ProductPageStyles.ts
+++ src/Styles/ProductPageStyles.ts
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 import { css } from "@emotion/react";
 
-// Emotion Css Template
-/*
+export const ProductsCont = styled.div`
+  @media (max-width: 800px) {
+  }
+  @media (max-width: 480px) {
+  }
+`;
+
+/* Css Template
 
 export const A = css`
+  
+  @media (max-width: 800px) {
+  }
+  @media (max-width: 480px) {
+  }
+`;
+
+export const ProductsCont = styled.div`
   
   @media (max-width: 800px) {
   }

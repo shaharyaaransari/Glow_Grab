53e8a58e92c0bb1830fc2ba53b5d1f38957784f5
unknown <rai2889@gmail.com>
2023-06-15 12:50:36 +0530

    cart structure

--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import ProductPage from "../Pages/ProductPage";
 import SingleProductPage from "../Pages/SingleProductPage";
+import Cart from "../Cart/Cart";
 
 const AllRoutes = () => {
   return (
@@ -10,6 +11,8 @@ const AllRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductPage />} />
       <Route path="/products/:id" element={<SingleProductPage />} />
+      <Route path="/cart" element={<Cart />} />
+
     </Routes>
   );
 };

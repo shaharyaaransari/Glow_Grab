5805aa50515e88a03ad097bb406c39ff4762cad2
Sakshi Parmar <sakshiparmarr123@gmail.com>
2023-06-15 22:54:09 +0530

    hellooooo

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleSignupClick = () => {
+    navigate("/signup"); // Redirect to the signup page
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <button onClick={handleSignupClick}>Signup</button> {/* Button to trigger the signup redirection */}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
 import ProductPage from "../Pages/ProductPage";
 import SingleProductPage from "../Pages/SingleProductPage";
+import SignupCard from "../Pages/SignUp";
 
 const AllRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AllRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductPage />} />
       <Route path="/products/:id" element={<SingleProductPage />} />
+      <Route path="/signup" element={<SignupCard />} />
     </Routes>
   );
 };

ec119c609cdb5f0f91640a44f75d5df27eb05a69
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2023-06-16 18:39:56 +0530

    Update

--- src/Components/ProductsList.tsx
+++ src/Components/ProductsList.tsx
@@ -1,46 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { DiscountPercent } from "../Functions/DiscountPercent";
-import { Box, Image, Text, Button, Badge } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Image,
+  Text,
+  Button,
+  Badge,
+  Center,
+  IconButton,
+  Tooltip,
+} from "@chakra-ui/react";
+import { NavLink, Navigate, useNavigate } from "react-router-dom";
 import { AiTwotoneStar } from "react-icons/ai";
+import { BsCartPlus, BsFillCartPlusFill } from "react-icons/bs";
 
 // Styles Import
-import * as css from "../Styles/ProductPageStyles";
-import { ProductListCont } from "../Styles/ProductListStyles";
+import * as css from "../Styles/ProductListStyles";
+import {
+  ProductListOuter,
+  ProductListCont,
+  NameTag,
+} from "../Styles/ProductListStyles";
 
 interface ProductsListType {
   Products: Array<any>;
 }
 
+interface NavigateType {
+  target: string;
+}
+
 const ProductsList = ({ Products }: ProductsListType) => {
+  const navigate = useNavigate();
+  const [imageLoading, setImageLoading] = useState(true);
+
+  const handleAddToCart = (id: number) => {};
+
   return (
-    <ProductListCont>
-      {Products.map((item, ind) => (
-        <NavLink to={`/product/${item.id}`} key={item.id}>
-          {/* <Text>{item.id}</Text> */}
-          <Image src={item.img} alt={item.name} />
-          <Text>{item.name}</Text>
-          <Box>
-            <Text>&#x20B9; {item.newPrice}</Text>
-            {item.newPrice != item.oldPrice && <s>&#x20B9; {item.oldPrice}</s>}
-            {item.newPrice != item.oldPrice && (
-              <Text>
-                {DiscountPercent(item.id, item.oldPrice, item.newPrice)}%
-              </Text>
-            )}
-          </Box>
+    <ProductListOuter>
+      <ProductListCont>
+        {Products.map((item, ind) => (
+          <Box key={item.id} css={css.CardOuter}>
+            <Box as={NavLink} to={`/product/${item.id}`} target="_blank">
+              {/* <Text>{item.id}</Text> */}
+              {imageLoading && <h1>Loading</h1>}
 
-          <Box>
-            <Badge variant="outline" colorScheme="green">
-              {item.rating} <Image as={AiTwotoneStar} />
-            </Badge>
-            <Text>Based on {item.review} reviews</Text>
+              <Image
+                onLoad={() => setImageLoading(false)}
+                src={item.img}
+                alt={item.name}
+                css={css.CardImageCss}
+              />
+              <NameTag>{item.name}</NameTag>
+              <Box css={css.PriceCont}>
+                <Text css={css.NewPriceCss}>&#x20B9; {item.newPrice}</Text>
+                {item.newPrice != item.oldPrice && (
+                  <Text css={css.OldPriceCss}>
+                    <s>&#x20B9; {item.oldPrice}</s>
+                  </Text>
+                )}
+                {item.newPrice != item.oldPrice && (
+                  <Text css={css.DiscountCss}>
+                    {DiscountPercent(item.id, item.oldPrice, item.newPrice)}%
+                  </Text>
+                )}
+              </Box>
+              <Box css={css.RatingAndReviewCont}>
+                <Badge css={css.RatingCss} variant="outline">
+                  {item.rating} <Image as={AiTwotoneStar} />
+                </Badge>
+                <Text css={css.ReviewCss}>Based on {item.review} reviews</Text>
+              </Box>
+            </Box>
+            <Box
+              onClick={() => handleAddToCart(item.id)}
+              css={css.AddToCartButton}
+            >
+              Add to Cart
+            </Box>
           </Box>
-
-          <Text>Add to Cart</Text>
-        </NavLink>
-      ))}
-    </ProductListCont>
+        ))}
+      </ProductListCont>
+    </ProductListOuter>
   );
 };
 
